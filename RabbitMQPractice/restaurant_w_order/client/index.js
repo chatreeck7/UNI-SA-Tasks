f5df9ec08e4a45d53925425c5b456e023f818779
chatreeck7 <chatree.devl@gmail.com>
2023-09-27 14:34:33 +0700

    fix(spacing): Save for edit spacing

--- RabbitMQPractice/restaurant_w_order/client/index.js
+++ RabbitMQPractice/restaurant_w_order/client/index.js
@@ -6,60 +6,62 @@ const bodyParser = require("body-parser");
 
 const app = express();
 
-app.set("views",path.join(__dirname,"views"));
-app.set("view engine","hbs");
+app.set("views", path.join(__dirname, "views"));
+app.set("view engine", "hbs");
 
 app.use(bodyParser.json());
-app.use(bodyParser.urlencoded({extended:false}));
+app.use(bodyParser.urlencoded({ extended: false }));
 
-app.get("/",(req,res)=>{
-    client.getAllMenu(null,(err,data)=>{
-        if(!err){
-            res.render("menu",{
-                results: data.menu
-            });
-        }
-    });
+app.get("/", (req, res) => {
+  client.getAllMenu(null, (err, data) => {
+    if (!err) {
+      res.render("menu", {
+        results: data.menu,
+      });
+    }
+  });
 });
 
-var amqp = require('amqplib/callback_api');
+// Import dependencies
+const amqp = require("amqplib/callback_api");
+const process = require("process");
+
+const url = process.env.CLOUDAMQP_URL || "amqp://localhost";
 
 app.post("/placeorder", (req, res) => {
-	//const updateMenuItem = {
-    var orderItem = {
-		id: req.body.id,
-		name: req.body.name,
-		quantity: req.body.quantity,
-	};
+  const orderItem = {
+    id: req.body.id,
+    name: req.body.name,
+    quantity: req.body.quantity,
+  };
 
-    // Send the order msg to RabbitMQ 
-    amqp.connect('amqp://localhost', function(error0, connection) {
-        if (error0) {
-            throw error0;
-        }
-        connection.createChannel(function(error1, channel) {
-            if (error1) {
-                throw error1;
-            }
-            var queue = 'order_queue';
-            //var msg = process.argv.slice(2).join(' ') || "Hello World!";
+  // Send the order msg to RabbitMQ
+  amqp.connect(url, function (error0, connection) {
+    if (error0) {
+      throw error0;
+    }
+    connection.createChannel(function (error1, channel) {
+      if (error1) {
+        throw error1;
+      }
+      var queue = "order_queue";
+      //var msg = process.argv.slice(2).join(' ') || "Hello World!";
 
-            channel.assertQueue(queue, {
-                durable: true
-            });
-            channel.sendToQueue(queue, Buffer.from(JSON.stringify(orderItem)), {
-                persistent: true
-            });
-                console.log(" [x] Sent '%s'", orderItem);
-            });
-        });
+      channel.assertQueue(queue, {
+        durable: true,
+      });
+      channel.sendToQueue(queue, Buffer.from(JSON.stringify(orderItem)), {
+        persistent: true,
+      });
+      console.log(" [x] Sent '%s'", orderItem);
     });
-    //console.log("update Item %s %s %d",updateMenuItem.id, req.body.name, req.body.quantity);
-
+  });
+});
+//console.log("update Item %s %s %d",updateMenuItem.id, req.body.name, req.body.quantity);
 
 const PORT = process.env.PORT || 3000;
-app.listen(PORT,()=>{
-    console.log("Server running at port %d",PORT);
+app.listen(PORT, () => {
+  console.log("Server running at port %d", PORT);
 });
 
 //var data = [{
